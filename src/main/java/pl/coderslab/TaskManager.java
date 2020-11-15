c3096525388c3d9fcf4f234faa944c514632800a
TheDariusz <thedariusz@gmail.com>
2020-11-15 23:12:16 +0100

    display menu method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -12,8 +12,19 @@ class TaskManager {
     public static final char FILE_DELIMITER = ',';
 
     public static void main(String[] args) {
-        String[][] tasks = getTasksFromFile();
+        //String[][] tasks = getTasksFromFile();
+        displayMenu();
 
+    }
+
+    private static void displayMenu() {
+        System.out.println(ConsoleColors.RESET);
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+        System.out.print(ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
     }
 
     private static String[][] getTasksFromFile() {
