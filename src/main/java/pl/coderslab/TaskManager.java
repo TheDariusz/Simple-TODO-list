4e55f5728518606f874e5d7360bd354755d60f99
TheDariusz <thedariusz@gmail.com>
2020-11-16 21:01:47 +0100

    add MENU_OPTIONS final array

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -10,6 +10,7 @@ class TaskManager {
 
     public static final String TASKS_FILE_DATABASE = "tasks.csv";
     public static final char FILE_DELIMITER = ',';
+    public static final String[] MENU_OPTIONS = {"add", "remove", "list", "exit"};
 
     public static void main(String[] args) {
         String[][] tasks = getTasksFromFile();
