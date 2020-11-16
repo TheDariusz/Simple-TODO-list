2bbefa4625d39ecbabe4c9aa518273f3c33da191
TheDariusz <thedariusz@gmail.com>
2020-11-16 21:03:01 +0100

    remove getTasksFromFile method from main add MENU_OPTIONS final array

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -10,28 +10,11 @@ class TaskManager {
 
     public static final String TASKS_FILE_DATABASE = "tasks.csv";
     public static final char FILE_DELIMITER = ',';
+    public static final String[] MENU_OPTIONS = {"add", "remove", "list", "exit"};
 
     public static void main(String[] args) {
         String[][] tasks = getTasksFromFile();
 
     }
 
-    private static String[][] getTasksFromFile() {
-        File file = new File(TASKS_FILE_DATABASE);
-        String[][] arrTasks = new String[0][0];
-        int numberOfLines=0;
-
-        try (Scanner scan = new Scanner(file)) {
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] oneTask = {"Simple task - very important", "2020-03-09", "true"}; //getTaskFromLine(line);
-                arrTasks = Arrays.copyOf(arrTasks,arrTasks.length+1);
-                arrTasks[numberOfLines]=oneTask;
-                numberOfLines++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return arrTasks;
-    }
 }
