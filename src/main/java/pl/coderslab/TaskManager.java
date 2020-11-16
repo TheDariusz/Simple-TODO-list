30f1c19427acd2a20d40f37dea9162e18e34378f
TheDariusz <thedariusz@gmail.com>
2020-11-16 21:00:49 +0100

    display menu method (switch to MENU_OPTIONS array)

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -10,6 +10,7 @@ class TaskManager {
 
     public static final String TASKS_FILE_DATABASE = "tasks.csv";
     public static final char FILE_DELIMITER = ',';
+    public static final String[] MENU_OPTIONS = {"add", "remove", "list", "exit"};
 
     public static void main(String[] args) {
         //String[][] tasks = getTasksFromFile();
@@ -21,28 +22,9 @@ class TaskManager {
         System.out.println(ConsoleColors.RESET);
         System.out.println(ConsoleColors.BLUE + "Please select an option:");
         System.out.print(ConsoleColors.RESET);
-        System.out.println("add");
-        System.out.println("remove");
-        System.out.println("list");
-        System.out.println("exit");
-    }
-
-    private static String[][] getTasksFromFile() {
-        File file = new File(TASKS_FILE_DATABASE);
-        String[][] arrTasks = new String[0][0];
-        int numberOfLines=0;
-
-        try (Scanner scan = new Scanner(file)) {
-            while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] oneTask = {"Simple task - very important", "2020-03-09", "true"}; //getTaskFromLine(line);
-                arrTasks = Arrays.copyOf(arrTasks,arrTasks.length+1);
-                arrTasks[numberOfLines]=oneTask;
-                numberOfLines++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (String menuItem : MENU_OPTIONS){
+            System.out.println(menuItem);
         }
-        return arrTasks;
     }
+
 }
