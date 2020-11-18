8b8478c80bb45635935023129cf2a28f8380c550
TheDariusz <thedariusz@gmail.com>
2020-11-18 21:34:07 +0100

    fixed all pr comments closes#3

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -8,33 +8,30 @@ import java.util.Scanner;
 
 class TaskManager {
 
-    public static final String TASKS_FILE_DATABASE = "tasks.csv";
-    public static final char FILE_DELIMITER = ',';
-    public static final String[] MENU_OPTIONS = {"add", "remove", "list", "exit"};
+    private static final String TASKS_FILE_DATABASE = "tasks.csv";
+    private static final String FILE_DELIMITER = ",";
 
     public static void main(String[] args) {
-        String[][] tasks = getTasksFromFile();
-
+        String[][] tasks = readTasksFromFile();
+        System.out.println(tasks);
     }
 
-    private static String[][] getTasksFromFile() {
+    private static String[][] readTasksFromFile() {
         File file = new File(TASKS_FILE_DATABASE);
         String[][] arrTasks = new String[0][0];
         int numberOfLines = 0;
 
         try (Scanner scan = new Scanner(file)) {
             while (scan.hasNextLine()) {
-                String line = scan.nextLine();
-                String[] oneTask = splitColumns(line);
+                String[] oneTask = createTask(scan.nextLine());
                 oneTask = addIndexAtBeginning(numberOfLines, oneTask);
                 arrTasks = Arrays.copyOf(arrTasks, arrTasks.length + 1);
                 arrTasks[numberOfLines] = oneTask;
                 numberOfLines++;
             }
         } catch (IOException e) {
+            System.out.format("Problems with data file! \n Please check file: %s", TASKS_FILE_DATABASE);
             e.printStackTrace();
-            System.out.println("Problems with data file!");
-            System.out.format("Please check file: %s", TASKS_FILE_DATABASE);
         }
         return arrTasks;
     }
@@ -48,7 +45,7 @@ class TaskManager {
         return lineWithIndex;
     }
 
-    private static String[] splitColumns(String line) {
-        return line.split(",");
+    private static String[] createTask(String line) {
+        return line.split(FILE_DELIMITER);
     }
 }
